164c36794d757a3672376e1e567880cb463c4faf
Imanshadowww <imanshadowww@atomicmail.io>
2026-09-09 21:09:44 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# محدود کردن پردازش‌های همزمان و لاگ‌ها برای پایین نگه‌داشتن CPU
 export GOMAXPROCS=1
 export GOMEMLIMIT=50MiB
 export TS_NO_LOGS_NO_SUPPORT=true
 
-echo "Starting tailscaled in balanced mode..."
+echo "Starting tailscaled in userspace mode..."
 ./tailscale_1.74.0_amd64/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock &
 
-# ۵ ثانیه استراحت تا میانگین مصرف پردازنده در لحظه اول بالا نرود
 sleep 5
 
-echo "Authenticating Tailscale..."
-./tailscale_1.74.0_amd64/tailscale --socket=/tmp/tailscaled.sock up --authkey="${TS_AUTHKEY}" --hostname=Wispbyte-Server --advertise-exit-node
+echo "Authenticating Tailscale with reset..."
+./tailscale_1.74.0_amd64/tailscale --socket=/tmp/tailscaled.sock up --reset --authkey="${TS_AUTHKEY}" --hostname=Wispbyte-Server --accept-dns=false
