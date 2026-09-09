78455b2187dc4b0548e3560f20aea57181679995
Imanshadowww <imanshadowww@atomicmail.io>
2026-09-09 20:52:13 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "Starting tailscaled in userspace mode..."
-./tailscale_1.74.0_amd64/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock &
+# محدود کردن شدید منابع برای فرار از سقف ۱۵ درصدی پردازنده
+export GOMAXPROCS=1
+export GOMEMLIMIT=50MiB
+export TS_NO_LOGS_NO_SUPPORT=true
+
+echo "Starting tailscaled in ultra-light mode..."
+./tailscale_1.74.0_amd64/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock --no-logs-no-support &
 sleep 3
 
 echo "Authenticating Tailscale..."
-./tailscale_1.74.0_amd64/tailscale --socket=/tmp/tailscaled.sock up --authkey="${TS_AUTHKEY}" --hostname=faable-server --advertise-exit-node
+./tailscale_1.74.0_amd64/tailscale --socket=/tmp/tailscaled.sock up --authkey="${TS_AUTHKEY}" --hostname=Wispbyte-Server --advertise-exit-node --accept-dns=false --shields-up
