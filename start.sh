5793a788f6ba74a19f8d78691eb62f3547e6cb07
Imanshadowww <imanshadowww@atomicmail.io>
2026-09-09 07:29:32 +0330

    Update start.sh
    Set GOMEMLIMIT to 100MiB and disable DNS acceptance.

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "Starting tailscaled in userspace mode..."
+export GOMEMLIMIT=100MiB
+
+echo "Starting tailscaled..."
 /tmp/tailscale_1.74.0_amd64/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 
 sleep 5
 
 echo "Authenticating Tailscale..."
-/tmp/tailscale_1.74.0_amd64/tailscale up --authkey="${TS_AUTHKEY}" --hostname=faable-server --advertise-exit-node
+/tmp/tailscale_1.74.0_amd64/tailscale up --authkey="${TS_AUTHKEY}" --hostname=faable-server --advertise-exit-node --accept-dns=false
